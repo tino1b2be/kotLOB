8f02253c57916da550348181826bd53517ce665e
Tinotenda Chemvura <ttchemvura@gmail.com>
2021-07-30 00:22:19 +0200

    added skeleton tests for the orderbook

---
+++ src/test/kotlin/OrderBookTest.kt
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Test
+
+class OrderBookTest {
+
+    @Test
+    fun testEmptyOrderBook() {
+
+    }
+
+    @Test
+    fun testAddOneBidOrder() {
+
+    }
+
+    @Test
+    fun testAddMultipleBidOrders() {
+    }
+
+    @Test
+    fun testAddOneAskOrder() {
+
+    }
+
+    @Test
+    fun testAddMultipleAskOrders() {
+    }
+
+    @Test
+    fun testSmallBidTrade() {
+
+    }
+
+    @Test
+    fun testLargeBidTrade() {
+
+    }
+
+    @Test
+    fun testSmallBidAndAskTrades() {
+
+    }
+
+    @Test
+    fun testLargeBidAndAskTrades(){
+
+
+    }
+
+    @Test
+    fun testEmptyTradeHistory(){
+
+    }
+
+    @Test
+    fun testSmallTradeHistory(){
+
+    }
+
+    @Test
+    fun testMaxTradeHistory(){
+
+    }
+
+    @Test
+    fun isEmpty() {
+
+    }
+
+}
